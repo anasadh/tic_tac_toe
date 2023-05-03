2992f318fb8a281366e84abe3dbe4b2dd3cc29eb
Anamika Sadh <97339793+anasadh@users.noreply.github.com>
2023-05-03 19:19:24 +0530

    Add files via upload

---
+++ tic_tac_toe.java
@@ -0,0 +1,126 @@
+import java.util.Scanner;
+
+public class tic_tac_toe {
+    private char[][] board;
+    private char currentPlayer;
+
+    public tic_tac_toe() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    public void printBoard() {
+        System.out.println("-------------");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    public boolean isBoardFull() {
+        boolean isFull = true;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    isFull = false;
+                }
+            }
+        }
+
+        return isFull;
+    }
+
+    public boolean checkForWin() {
+        return (checkRowsForWin() || checkColumnsForWin() || checkDiagonalsForWin());
+    }
+
+    private boolean checkRowsForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (checkRowCol(board[i][0], board[i][1], board[i][2])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkColumnsForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (checkRowCol(board[0][i], board[1][i], board[2][i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonalsForWin() {
+        return ((checkRowCol(board[0][0], board[1][1], board[2][2])) || (checkRowCol(board[0][2], board[1][1], board[2][0])));
+    }
+
+    private boolean checkRowCol(char c1, char c2, char c3) {
+        return ((c1 != '-') && (c1 == c2) && (c2 == c3));
+    }
+
+    public void changePlayer() {
+        if (currentPlayer == 'X') {
+            currentPlayer = 'O';
+        } else {
+            currentPlayer = 'X';
+        }
+    }
+
+    public boolean placeMark(int row, int col) {
+        if ((row >= 0) && (row < 3)) {
+            if ((col >= 0) && (col < 3)) {
+                if (board[row][col] == '-') {
+                    board[row][col] = currentPlayer;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        tic_tac_toe game = new tic_tac_toe();
+        game.printBoard();
+
+        while (!game.isBoardFull() && !game.checkForWin()) {
+            System.out.println("Player " + game.currentPlayer + ", enter row (0-2): ");
+            int row = scanner.nextInt();
+            System.out.println("Player " + game.currentPlayer + ", enter column (0-2): ");
+            int col = scanner.nextInt();
+            if (game.placeMark(row, col)) {
+                game.printBoard();
+                game.changePlayer();
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+        }
+    
+        if (game.checkForWin()) {
+            game.changePlayer();
+            System.out.println("Congratulations Player " + game.currentPlayer + "! You win!");
+        } else {
+            System.out.println("It's a tie!");
+        }
+    
+        scanner.close();
+    }
+    
+}
